5dc6639c4d64018c13d8c32cf4a6e16aacfc74f1
Robert <rkinney333@gmail.com>
2022-08-13 19:51:41 -0400

    Added Bulma css, esLinting, and unit testing

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,11 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+
+  // const component: DefineComponent<{}, {}, any>;
+
+  const component: DefineComponent<string, unknown>;
+
+  export default component;
 }
